09c1ed7a31dbad8dca841372943c3c0e8bf2f6c8
Sebastian Grossmann <sebasgrossmann@Sebastians-MacBook-Pro.local>
2022-08-22 16:52:03 -0400

    Receiving message route

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const twilio = require('twilio');
+require('dotenv').config();
 
-const accountSid = process.env.accountSid; // Your Account SID from www.twilio.com/console
-const authToken = process.env.authToken; // Your Auth Token from www.twilio.com/console
+var app = express();
 
-const client = new twilio(accountSid, authToken);
+app.use(bodyParser.urlencoded({extended: false}))
 
-client.messages
-  .create({
-    body: 'Hello from Node',
-    to: '+17864737399', // Text this number
-    from: '+13858538002', // From a valid Twilio number
-  })
-  .then((message) => console.log(message.sid))
-  .catch((e) => console.log(e))
+//Send message after recieving message
+app.post('/message', function (req, res){
+    console.log(req.body)
+    var msgFrom = req.body.From
+    var msgBody = req.body.Body
+
+    res.send(`
+    <Response>
+        <Message>
+            Hello from ${msgFrom}, you said: ${msgBody}
+        </Message>
+    </Response>
+
+    `);
+});
+
+app.listen(3000, function(){
+    console.log("App listening port 3000")
+});
+
+//Send message
+// const accountSid = process.env.accountSid; // Your Account SID from www.twilio.com/console
+// const authToken = process.env.authToken; // Your Auth Token from www.twilio.com/console
+
+// const client = new twilio(accountSid, authToken);
+
+// client.messages
+//   .create({
+//     body: 'Hello from Node',
+//     to: '+17864737399', // Text this number
+//     from: '+13858538002', // From a valid Twilio number
+//   })
+//   .then((message) => console.log(message.sid))
+//   .catch((e) => console.log(e))
 
